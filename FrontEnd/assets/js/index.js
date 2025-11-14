97d1b6ab067d64543b559ea60e085961b3e5a2c1
Floane VARATHEN <fvarathen@gmail.com>
2025-11-14 14:31:40 +0100

    feat: ajout des modales Galeries et Formulaire
    - Création de la structure HTML des modales
    - Ajout des fonctions JS (ouverture de la modale, fermeture via la croix ou l'overlay, swicth entre les deux modales

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -16,17 +16,66 @@ if (token) {
         loginLink.addEventListener("click", (e) => {
             e.preventDefault();
             localStorage.removeItem("authToken");
-            localStorage.removeItem("isLoggedIn");
             window.location.reload();
         });
+
     } else {
         if (banner) banner.style.display = "none";
         if (editBtn) editBtn.style.display = "none";
         if (filtersEdition) filtersEdition.style.display = "flex";
-
-        if(loginLink) {
-            loginLink.textContent = "login";
-            loginLink.href = "login.html";
-        }
     }
 }
+
+const dlg = document.getElementById("works-modal");
+const btnOpen = document.getElementById("edit-btn");
+const btnClose = document.getElementById("modal-close");
+const btnBack = document.getElementById("modal-back");
+const viewGal = document.getElementById("modal-view-gallery");
+const viewForm = document.getElementById("modal-view-form");
+const btnOpenAdd = document.getElementById("btn-open-add");
+const title = document.getElementById("modal-title");
+const uploadTrigger = document.getElementById("upload-trigger");
+const uploadInput = document.getElementById("upload-input");
+const uploadPreview = document.getElementById("upload-preview");
+
+function showGallery(){
+    title.textContent = "Galerie photo";
+    viewGal.hidden = false; 
+    viewGal.setAttribute("aria-hidden","false");
+    viewForm.hidden = true;
+    viewForm.setAttribute("aria-hidden", "true");
+    btnBack.hidden = true;
+}
+
+function showForm() {
+    title.textContent = "Ajout photo";
+    viewGal.hidden = true; 
+    viewGal.setAttribute("aria-hidden","true");
+    viewForm.hidden = false;
+    viewForm.setAttribute("aria-hidden", "false");
+    btnBack.hidden = false;
+}
+
+editBtn.addEventListener ("click", () => {
+    dlg.showModal();
+    showGallery();
+});
+
+btnOpenAdd.addEventListener ("click", () => {
+    showForm();
+});
+
+btnBack.addEventListener("click", () => {
+    showGallery();
+});
+
+btnClose.addEventListener("click", () => {
+    dlg.close();
+});
+
+dlg.addEventListener("click", (event) => {
+    const panel = dlg.querySelector(".modal_panel");
+    if (!panel) return;
+    const clickInside = panel.contains(event.target);
+    if (!clickInside) dlg.close();
+});
